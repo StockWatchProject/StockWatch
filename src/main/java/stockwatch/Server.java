b6d9336adeee685c0d3f7f28343ed3d919192de6
rotkiewi <rotkiewi@konradek.nsn-intra.net>
2011-09-13 11:48:30 +0200

    little fix for previous commit

--- src/main/java/stockwatch/Server.java
+++ src/main/java/stockwatch/Server.java
@@ -7,9 +7,6 @@ public class Server {
     private static final int INIT_DELAY = 0;
 
     public static void main(String args[]) {
-    	System.setProperty( "proxySet", "true" );
-    	System.setProperty( "http.proxyHost", "10.144.1.10" );
-    	System.setProperty( "http.proxyPort", Integer.toString( 8080 ) );
         Timer timer = new Timer();
         // refresh quotes every 5 minutes
         timer.schedule(new StockMarket(), INIT_DELAY, REPEAT_AFTER);
